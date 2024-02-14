0442f08aa3ad837f5723c5c20a85a06c72a40a23
siaosiaogewun <siaosiaogewun@gmail.com>
2024-02-14 16:49:42 +0800

    Update 05tronweb转账usdt001.js
    更新usdt转账代码

--- 05tronweb转账usdt001.js
+++ 05tronweb转账usdt001.js
@@ -1,24 +1,30 @@
 const TronWeb = require('tronweb');
 const HttpProvider = TronWeb.providers.HttpProvider;
 const fullNode = new HttpProvider("https://api.shasta.trongrid.io");
+// const fullNode = new HttpProvider("http://192.168.1.162:8090");
 const solidityNode = new HttpProvider("https://api.shasta.trongrid.io");
 const eventServer = new HttpProvider("https://api.shasta.trongrid.io");
 const privateKey = "8e4ba713657f8026b31e98707464f7de0d455f324b32f906c53010e6dfed17f2";
 const tronWeb = new TronWeb(fullNode, solidityNode, eventServer, privateKey);
 
-const ACCOUNT = "TTzPiwbBedv7E8p4FkyPyeqq4RVoqRL3TW";
-const memo = "tttttttttttttttransfer";
+
+const CONTRACT = "TG3XXyExBkPp9nzdajDZsozEu4BkaSJozs";
+
+const ACCOUNT = "TEQH6py1Pi8YHNgi9cPMHCKLboBTUZrsYT";
 
 async function main() {
+    const {
+        abi
+    } = await tronWeb.trx.getContract(CONTRACT);
+    // console.log(JSON.stringify(abi));
 
-    console.log(tronWeb.defaultAddress.base58, "=>", ACCOUNT);
+    const contract = tronWeb.contract(abi.entrys, CONTRACT);
 
-    const unSignedTxn = await tronWeb.transactionBuilder.sendTrx(ACCOUNT, 1000);
-    const unSignedTxnWithNote = await tronWeb.transactionBuilder.addUpdateData(unSignedTxn, memo, 'utf8');
-    const signedTxn = await tronWeb.trx.sign(unSignedTxnWithNote);
-    console.log("signed =>", signedTxn);
-    const ret = await tronWeb.trx.sendRawTransaction(signedTxn);
-    console.log("broadcast =>", ret);
+    const balance = await contract.methods.balanceOf(ACCOUNT).call();
+    console.log("balance:", balance.toString());
+
+    const resp = await contract.methods.transfer(ACCOUNT, 1000).send();
+    console.log("transfer:", resp);
 }
 
 main().then(() => {
@@ -27,3 +33,5 @@ main().then(() => {
     .catch((err) => {
         console.log("error:", err);
     });
+
+//验证代码运行成功
