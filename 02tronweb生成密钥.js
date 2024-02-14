b8a0475179c304a9068899b5c717c09d12749d1d
siaosiaogewun <siaosiaogewun@gmail.com>
2024-02-14 15:19:33 +0800

    v0.1
    整理更新

--- 02tronweb生成密钥.js
+++ 02tronweb生成密钥.js
@@ -4,8 +4,8 @@ const TronWeb = require('tronweb');
 // 助记词
 const mnemonic = 'diary steak forest resist pumpkin grass outer punch bone saddle method umbrella';
 
-// 创建两个可写流，分别指定输出文件为output_json_format.txt和output_plain_format.txt
-const outputStreamJsonFormat = fs.createWriteStream('output_json_format.txt');
+// 创建两个可写流，分别指定输出文件为output_json_format_without_prefix.txt和output_plain_format.txt
+const outputStreamJsonFormatWithoutPrefix = fs.createWriteStream('output_json_format_without_prefix.txt');
 const outputStreamPlainFormat = fs.createWriteStream('output_plain_format.txt');
 
 // 循环生成私钥并保存
@@ -15,21 +15,21 @@ for (let i = 0; i <= 100; i++) {
     const privateKeyWithPrefix = privateKeyObject.privateKey;
     const privateKeyWithoutPrefix = privateKeyWithPrefix.slice(2); // 移除私钥开头的 "0x"
 
-    // 将带有前缀的私钥以JSON格式写入文件
-    const outputStringJsonFormat = `{"PrivateKey": "${privateKeyWithPrefix}"},\n`;
-    outputStreamJsonFormat.write(outputStringJsonFormat);
+    // 生成 JSON 格式的私钥字符串（去掉 "0x" 前缀）
+    const outputStringJsonFormatWithoutPrefix = `{"PrivateKey": "${privateKeyWithoutPrefix}"},\n`;
+    outputStreamJsonFormatWithoutPrefix.write(outputStringJsonFormatWithoutPrefix);
 
-    // 将不带前缀的私钥写入文件
+    // 生成纯私钥字符串（不带 "0x" 前缀）
     const outputStringPlainFormat = `${privateKeyWithoutPrefix}\n`;
     outputStreamPlainFormat.write(outputStringPlainFormat);
 
     // 同时在控制台输出
-    console.log(outputStringJsonFormat);
+    console.log(outputStringJsonFormatWithoutPrefix);
 }
 
 // 关闭文件流
-outputStreamJsonFormat.end();
+outputStreamJsonFormatWithoutPrefix.end();
 outputStreamPlainFormat.end();
 
-console.log('Output with JSON format saved to output_json_format.txt');
+console.log('Output with JSON format (without "0x" prefix) saved to output_json_format_without_prefix.txt');
 console.log('Output with plain format saved to output_plain_format.txt');
