9820a2ac46322b68db093655041ec3bade5ea585
siaosiaogewun <siaosiaogewun@gmail.com>
2024-02-14 14:33:20 +0800

    Create tronweb查余额.js
    更新查询js查询余额

---
+++ tronweb查余额.js
@@ -0,0 +1,31 @@
+const TronWeb = require('tronweb');
+
+const tronWeb = new TronWeb({
+    fullHost: 'https://api.trongrid.io',
+    solidityNode: 'https://api.trongrid.io',
+    eventServer: 'https://api.trongrid.io',
+});
+
+tronWeb.setAddress('TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t');
+
+const tokenContractAddress = 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t';
+const accountAddress = 'TNPeeaaFB7K9cmo4uQpcU32zGK8G1NYqeL';
+
+const getTRC20TokenBalance = async (contractAddress, account) => {
+    try {
+        const accountHex = tronWeb.address.toHex(account);
+        const contract = await tronWeb.contract().at(contractAddress);
+        const balance = await contract.balanceOf(accountHex).call();
+        return tronWeb.toDecimal(balance._hex);
+    } catch (error) {
+        console.error('Error al obtener el balance de USDT:', error);
+        throw error;
+    }
+};
+
+(async () => {
+    const balance = await getTRC20TokenBalance(tokenContractAddress, accountAddress);
+    console.log(balance / 1000000);
+})();
+
+
