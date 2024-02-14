52fee01d88bfcd0d8d0d47a41217277fd5af28aa
siaosiaogewun <siaosiaogewun@gmail.com>
2024-02-14 15:01:04 +0800

    Update 生成密钥.js
    生成两种格式的密钥txt文件，方便归集程序调用。

--- 生成密钥.js
+++ 生成密钥.js
@@ -4,23 +4,32 @@ const TronWeb = require('tronweb');
 // 助记词
 const mnemonic = 'diary steak forest resist pumpkin grass outer punch bone saddle method umbrella';
 
-// 创建一个可写流，指定输出文件为output.txt
-const outputStream = fs.createWriteStream('output.txt');
+// 创建两个可写流，分别指定输出文件为output_json_format.txt和output_plain_format.txt
+const outputStreamJsonFormat = fs.createWriteStream('output_json_format.txt');
+const outputStreamPlainFormat = fs.createWriteStream('output_plain_format.txt');
 
-// 循环生成私钥和地址并打印
+// 循环生成私钥并保存
 for (let i = 0; i <= 100; i++) {
     const path = `m/44'/195'/0'/0/${i}`;
     const privateKeyObject = TronWeb.fromMnemonic(mnemonic, path);
-    const privateKey = privateKeyObject.privateKey.slice(2); // 移除私钥开头的 "0x"
-    const outputString = `{"PrivateKey": "${privateKey}"},\n`;
+    const privateKeyWithPrefix = privateKeyObject.privateKey;
+    const privateKeyWithoutPrefix = privateKeyWithPrefix.slice(2); // 移除私钥开头的 "0x"
 
-    // 将结果写入文件
-    outputStream.write(outputString);
+    // 将带有前缀的私钥以JSON格式写入文件
+    const outputStringJsonFormat = `{"PrivateKey": "${privateKeyWithPrefix}"},\n`;
+    outputStreamJsonFormat.write(outputStringJsonFormat);
+
+    // 将不带前缀的私钥写入文件
+    const outputStringPlainFormat = `${privateKeyWithoutPrefix}\n`;
+    outputStreamPlainFormat.write(outputStringPlainFormat);
 
     // 同时在控制台输出
-    console.log(outputString);
+    console.log(outputStringJsonFormat);
 }
 
 // 关闭文件流
-outputStream.end();
-console.log('Output saved to output.txt');
+outputStreamJsonFormat.end();
+outputStreamPlainFormat.end();
+
+console.log('Output with JSON format saved to output_json_format.txt');
+console.log('Output with plain format saved to output_plain_format.txt');
